d3fb7261ca37228cf64d7f3db5ad1c9e65eddbf4
Sleroq <hireme@sleroq.link>
2024-08-25 19:30:33 +0300

    Lots of refactoring and some bugfixes

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,5 +1,10 @@
-import { RequestMessage } from '../ai-api.ts';
-import { ChatMessage } from '../main.ts';
+import {
+    Message,
+    ReplyMessage,
+    Sticker,
+    Update,
+} from 'https://deno.land/x/grammy@v1.14.1/types.deno.ts';
+import { ChatMessage } from './memory.ts';
 
 interface Environment {
     AI: {
@@ -23,8 +28,7 @@ const DEFAULT_URL = 'https://api.openai.com/v1/chat/completions';
 const DEFAULT_PROMPT = `
 You are a bot Слюша, created by @sleroq. Help other people and answer funny.
 `;
-const DEFAULT_FINAL_PROMPT =
-    'Answer should consist only from YOUR (Слюша) reply.';
+const DEFAULT_FINAL_PROMPT = 'Answer must only contain have your reply text.';
 
 const DEFAULT_CONTEXT_LIMIT = 40;
 const DEFAULT_CONTEXT_TIMEOUT = 60 * 24 * 7; // In minutes
@@ -71,37 +75,128 @@ export function resolveEnv(): Environment {
     };
 }
 
-export function assembleHistory(history: ChatMessage[]): RequestMessage[] {
-    const messages: RequestMessage[] = [];
-
-    for (let i = 0; i < history.length; i++) {
-        const message = history[i];
-        let context = '';
-
-        if (message.replyTo && !message.sender.myself) {
-            // Add original message if this is last message in history
-            if (i == history.length - 1) {
-                context +=
-                    `(Reply to: ${message.replyTo.sender.name}): > ${message.replyTo.text}\n`;
-            } else {
-                context += `(Reply to: ${message.replyTo.sender.name}):\n`;
-            }
-        }
-        context +=
-            `${message.sender.name} (@${message.sender.username}): ${message.text}`;
-
-        if (message.sender.myself) {
-            messages.push({ role: 'assistant', content: context });
-        } else {
-            messages.push({ role: 'user', content: context });
-        }
-    }
-
-    return messages;
-}
-
 export function getRandomInt(min: number, max: number) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+export function stickerToText({ emoji }: Sticker): string {
+    return emoji ? `[Sticker ${emoji}]` : '[Sticker]';
+}
+
+export function removeBotName(
+    response: string,
+    name: string,
+    username: string,
+) {
+    const regexFull = new RegExp(`^${name}\\s*\\(@${username}\\):\\s*`, 'gmi');
+    const regexName = new RegExp(`^${name}\\s*:\\s*`, 'gmi');
+    response = response.replace(regexFull, '');
+    return response.replace(regexName, '');
+}
+
+export function sliceMessage(message: string, maxLength: number): string {
+    return message.length > maxLength
+        ? message.slice(0, maxLength) + '...'
+        : message;
+}
+
+interface HistoryOptions {
+    simbolLimit?: number;
+    usernames?: boolean;
+}
+
+export function makeHistoryString(
+    history: ChatMessage[],
+    { simbolLimit, usernames }: HistoryOptions = {},
+): string {
+    if (!simbolLimit) simbolLimit = 1000;
+    if (!usernames) usernames = true;
+
+    let result = '';
+    for (let i = 0; i < history.length; i++) {
+        const message = history[i];
+        let context = `${message.sender.name} (@${message.sender.username}): `;
+        if (!usernames) {
+            context = `${message.sender.name}: `;
+        }
+
+        if (message.replyTo && !message.sender.myself) {
+            // Add original message if this is last message in history
+            if (i == history.length - 1) {
+                const replyText = sliceMessage(
+                    message.replyTo.text,
+                    simbolLimit,
+                );
+                context +=
+                    `(in reply to: ${message.replyTo.sender.name} > "${replyText}"): `;
+            } else {
+                context += `(in reply to: ${message.replyTo.sender.name}): `;
+            }
+        }
+
+        context += sliceMessage(message.text, simbolLimit);
+
+        result += context + '\n';
+    }
+
+    return result;
+}
+
+export function getText(
+    msg: (Message & Update.NonChannel) | ReplyMessage,
+    botId: number,
+) {
+    let text = msg.text || '';
+
+    if (msg.sticker) {
+        text += stickerToText(msg.sticker);
+    }
+
+    if (msg.caption) {
+        text += msg.caption;
+    }
+
+    // Replace new lines with spaces
+    text = text.replace(/[\n\r]/g, ' ');
+
+    if (!text.trim()) return;
+
+    if (msg.forward_from && msg.forward_from.id !== botId) {
+        const from = msg.forward_from.first_name.slice(0, 20);
+        text = `(forwarded from ${from}): ${text}`;
+    }
+    if (msg.forward_from_chat && 'title' in msg.forward_from_chat) {
+        const from = msg.forward_from_chat.title.slice(0, 20);
+        text = `(forwarded from ${from}): ${text}`;
+    }
+
+    const attachments: (keyof (Message & Update.NonChannel))[] = [
+        'photo',
+        'video',
+        'animation',
+        'audio',
+        'voice',
+        'document',
+        'video_note',
+        'contact',
+        'location',
+        'venue',
+        'poll',
+        'dice',
+        'game',
+    ];
+
+    const attachmentsText = attachments.reduce((acc, key) => {
+        if (msg[key]) {
+            acc += ` [${key}]`;
+        }
+
+        return acc;
+    }, '');
+
+    text += attachmentsText;
+
+    return text;
 }
--- main.ts
+++ main.ts
@@ -1,18 +1,19 @@
 import { Bot, Context } from 'https://deno.land/x/grammy@v1.14.1/mod.ts';
-import AIApi, { RequestMessage } from './ai-api.ts';
+import AIApi from './ai-api.ts';
 import Werror from './lib/werror.ts';
 import { delay } from 'https://deno.land/std@0.177.0/async/mod.ts';
 import { limit } from 'https://deno.land/x/grammy_ratelimiter@v1.2.0/mod.ts';
-import { assembleHistory, getRandomInt, resolveEnv } from './lib/helpers.ts';
+import {
+    getRandomInt,
+    getText,
+    removeBotName,
+    resolveEnv,
+    sliceMessage,
+} from './lib/helpers.ts';
 import {
     Message,
     Update,
 } from 'https://deno.land/x/grammy@v1.14.1/types.deno.ts';
-import Logger from 'https://deno.land/x/logger@v1.1.1/logger.ts';
-
-const logger = new Logger();
-await logger.initFileLogger('log', { rotate: true });
-
 import {
     ChatMessage,
     getChat,
@@ -20,6 +21,7 @@ import {
     Memory,
     saveMemory,
 } from './lib/memory.ts';
+import logger from './lib/logger.ts';
 
 // TODO: move this chit to config
 const NAMES = [
@@ -52,6 +54,7 @@ const nepons = [
     'нехочу отвечать щас чето',
     'подумаю, может потом тебе скажу',
 ];
+
 const tendToIgnore = [
     'ор+',
     'ору+',
@@ -85,6 +88,8 @@ const tendToIgnore = [
 const memory: Memory = await loadMemory();
 const env = resolveEnv();
 
+let makingNotes = false;
+
 interface RequestInfo {
     isRandom: boolean;
 }
@@ -101,82 +106,151 @@ const Api = new AIApi(
     env.AI.model,
     env.AI.token,
 );
-
-bot.use(limit());
 bot.catch((error) => logger.error(error));
+
 bot.on('message', async (ctx, next) => {
-    const chat = getChat(chats, ctx.chat.id, ctx.msg.message_id, ctx.chat.type);
+    const chat = getChat(
+        memory,
+        ctx.chat.id,
+        ctx.chat.type,
+    );
     const message = handleMessage(ctx.msg);
     if (!message) return;
 
-    let history = chat.history;
-    history.push(message);
-
-    // Filter out irrelevant messages
-    history = history.filter((el) =>
-        new Date().getDate() - el.date < env.contextTimeout * 60 * 1000
-    );
+    const history = chat.history;
 
     // Make sure history is not too long
-    while (history.length > env.contextLimit) {
+    while (history.length > 80) {
         history.shift();
     }
 
+    history.push(message);
+
     chat.history = history;
+
+    // Save memory every 20 messages
+    if (memory.unsavedMsgsCount > 20) {
+        try {
+            await saveMemory(memory);
+        } catch (error) {
+            throw new Werror(error, 'Saving memory');
+        }
+
+        memory.unsavedMsgsCount = 0;
+
+        logger.info('Memory saved');
+    } else {
+        memory.unsavedMsgsCount++;
+    }
+
+    ctx.info = { isRandom: false };
 
     await next();
 });
 
 bot.command('start', (ctx) => ctx.reply('Привет! Я Слюша, бот-гений.'));
 
-bot.command('summary', async (ctx) => {
-    // Final system prompt
-    const prompt = `
-        !!Do not answer as Слюша!!.
-        Just write a concise summary of all the events that have occurred in this chat in Russian language.
-        Structure your response in a way that is easy to read, possibly with division into parts with new lines.
-        Fit into 2000 characters
-    `;
+// Make summary and notes every X messages
+bot.on('message', async (ctx, next) => {
+    const chat = getChat(
+        memory,
+        ctx.chat.id,
+        ctx.chat.type,
+    );
 
-    const chat = getChat(chats, ctx.chat.id, ctx.msg.message_id, ctx.chat.type);
-    const messages = assembleHistory(chat.history);
+    const usedDaysAgo = (Date.now() - chat.lastUse) / 1000 / 60 / 60 / 24;
 
-    let response;
-    try {
-        response = await Api.chatReply(
-            prompt,
-            messages,
-            chat.notes,
-            logger,
-        );
-    } catch (error) {
-        await ctx.reply('Что-то у меня не получилось', {
-            reply_to_message_id: ctx.msg.message_id,
-        });
-        throw new Werror(error, 'Getting summary from ai');
+    // Make less notes if bot is not used often
+    let frequency = 80;
+    if (usedDaysAgo > 2) {
+        frequency = usedDaysAgo ** 2 * 20;
     }
+
+    if (chat.lastNotes < frequency || makingNotes) {
+        chat.lastNotes++;
+        await next();
+        return;
+    }
+
+    const makeNotes = async () => {
+        makingNotes = true;
+
+        const historyPortion = chat.history.slice(-80);
+
+        const start = Date.now();
+        try {
+            const notes = await Api.makeNotes(historyPortion);
+            chat.notes.push(notes);
+            chat.lastNotes = 0;
+        } catch (error) {
+            logger.error('Could not make notes', error);
+        }
+        logger.info('Notes made in', (Date.now() - start) / 1000, 'seconds');
+
+        // Keep only 10 notes
+        while (chat.notes.length > 20) {
+            chat.notes.shift();
+        }
+
+        makingNotes = false;
+    };
+
+    void makeNotes();
+
+    await next();
+});
+
+bot.command('summary', async (ctx) => {
+    const chat = getChat(
+        memory,
+        ctx.chat.id,
+        ctx.chat.type,
+    );
+
+    if (chat.notes.length === 0) {
+        await ctx.reply('Рановато еще, надо накопить больше сообщений', {
+            reply_to_message_id: ctx.msg?.message_id,
+        });
+        return;
+    }
+
+    const response = chat.notes.slice(-2).join('\n');
 
     logger.info('/summary', response);
 
-    return replyWithMarkdown(ctx, response);
+    await replyWithMarkdown(ctx, response);
+
+    chat.history.push({
+        sender: {
+            id: bot.botInfo.id,
+            name: bot.botInfo.first_name,
+            username: bot.botInfo.username,
+            myself: true,
+        },
+        date: Date.now(),
+        text: response,
+        isSummary: true,
+    });
+
+    chat.lastUse = Date.now();
 });
 
 bot.on('message', async (ctx, next) => {
     const message = handleMessage(ctx.msg);
     if (!message) return;
 
-    const chat = getChat(chats, ctx.chat.id, ctx.msg.message_id, ctx.chat.type);
-
-    // Skipping queue for long messages
-    // Because they are more likely to be complete requests
-    if (message.text.length > 35) {
-        return next();
-    }
+    const chat = getChat(
+        memory,
+        ctx.chat.id,
+        ctx.chat.type,
+    );
 
     const mustReply = shouldReply(
+        chat,
         message.text,
         message.replyTo?.sender.id,
         ctx.chat.type,
+        message.replyTo?.id,
     );
     const randomReply = getRandomInt(0, 100) > 100 - env.randomReply;
 
@@ -185,8 +259,9 @@ bot.on('message', async (ctx, next) => {
 
     if (randomReply) {
         ctx.info.isRandom = true;
+        logger.info('Random reply');
         await next();
-    } else {
+    } else if (mustReply) {
         queue();
     }
 
@@ -204,13 +279,25 @@ bot.on('message', async (ctx, next) => {
             );
 
             const laterMustReply = userMsg.find((el) =>
-                shouldReply(el.text, el.replyTo?.sender.id, ctx.chat.type) &&
+                shouldReply(
+                    chat,
+                    el.text,
+                    el.replyTo?.sender.id,
+                    ctx.chat.type,
+                    el.replyTo?.isSummary,
+                ) &&
                 el.date > ctx.msg.date
             );
+
+            // TODO: Implement per-chat rate-limiting
 
             // Skipping, because will reply later anyway
             if (laterMustReply) {
                 typer.stop();
+                logger.info(
+                    'Skipping message, will reply later anyway',
+                    message?.text,
+                );
                 return;
             }
 
@@ -220,107 +307,133 @@ bot.on('message', async (ctx, next) => {
                 lastUserMsg.date > ctx.msg.date &&
                 ((Date.now() / 1000 - lastUserMsg.date) < 3)
             ) {
+                logger.info(
+                    'User is typing, waiting for the message to be sent',
+                );
                 queue();
                 return;
             }
-        }, 3000);
+
+            if (mustReply) {
+                logger.info('Must reply', message?.text);
+                try {
+                    await next();
+                } catch (error) {
+                    // Must handle error here, because queue runs without await
+                    console.log(error);
+                    typer.stop();
+                    return;
+                }
+            } else {
+                logger.info(
+                    'No reply, no if was triggered, skipping',
+                    message?.text,
+                    lastUserMsg.text,
+                    ctx.msg.date > lastUserMsg.date,
+                    (Date.now() / 1000 - lastUserMsg.date) < 3,
+                    mustReply,
+                    'mustReply',
+                );
+            }
+        }, 4000);
     }
 });
 
-async function makeNotes(
-    chat: Chat,
-    msgId: number,
-    messages: RequestMessage[],
-    prompt: string,
-) {
-    logger.info('generating notes');
+bot.use(limit(
+    {
+        // Allow only 2 messages to be handled every 5 seconds.
+        timeFrame: 5000,
+        limit: 2,
 
-    try {
-        chat.notes = await genNotes(
-            chat.notes,
-            messages,
-            prompt,
-        );
-    } catch (error) {
-        throw new Werror(error, 'Taking notes');
-    }
+        // This is called when the limit is exceeded.
+        onLimitExceeded: () => {
+            logger.warn('Rate limit exceeded');
+        },
 
-    logger.info('Chat notes:', chat.notes);
+        keyGenerator: (ctx) => {
+            if (ctx.hasChatType(['group', 'supergroup'])) {
+                return ctx.chat.id.toString();
+            }
 
-    chat.lastNotes = msgId;
-
-    try {
-        await saveMemory(chats);
-    } catch (error) {
-        throw new Werror(error, 'Saving memory');
-    }
-}
+            return ctx.from?.id.toString();
+        },
+    },
+));
 
 bot.on('message', async (ctx) => {
     const message = handleMessage(ctx.msg);
     if (!message) return;
 
-    const chat = getChat(chats, ctx.chat.id, ctx.msg.message_id, ctx.chat.type);
+    const chat = getChat(
+        memory,
+        ctx.chat.id,
+        ctx.chat.type,
+    );
     const history = chat.history;
-
-    const messages: RequestMessage[] = assembleHistory(history);
-
-    // Each time we add 2 messages to history.
-    // If contextLimit is not even, we should account to that
-    const shouldGenSummary =
-        ctx.msg.message_id - chat.lastNotes > env.contextLimit;
-    const summaryPrompt = env.AI.notesPrompt;
-
-    if (shouldGenSummary && summaryPrompt) {
-        try {
-            await makeNotes(chat, ctx.msg.message_id, messages, summaryPrompt);
-        } catch (error) {
-            logger.warn('Unable to take notes', error);
-        }
-    }
-
-    // Final system prompt
-    const prompt = `
-            Write your reply only to this message from ${message.sender.name} (@${message.sender.username}).
-            > ${message.text}
-            ${env.AI.finalPrompt}`;
-
-    logger.info(prompt);
 
     let response: string | undefined;
     let error;
+
+    const lastNote = chat.notes.slice(-4).join('\n');
+
+    const time = new Date().getTime();
     for (let i = 0; i < 2; i++) {
         try {
             response = await Api.chatReply(
-                prompt,
-                messages,
-                chat.notes,
-                logger,
+                history,
+                lastNote,
             );
             break;
         } catch (err) {
             error = err;
-            await delay(2000);
+            await delay(7000);
         }
     }
+    logger.info('Time to get response:', (new Date().getTime() - time) / 1000);
 
     if (!response) {
         if (!ctx.info.isRandom) {
+            logger.error('Unable to get response: ', error);
+
+            if (error instanceof Error) {
+                if (error.message === 'Safety violation detected in response') {
+                    await ctx.reply('Хочется тебя оскорбить, но я не могу', {
+                        reply_to_message_id: ctx.msg.message_id,
+                    });
+                    return;
+                }
+                if (
+                    error.message === 'No response candidates received from api'
+                ) {
+                    await ctx.reply('Извини, я не понял тебя', {
+                        reply_to_message_id: ctx.msg.message_id,
+                    });
+                    return;
+                }
+            }
+
             const idk = nepons[Math.floor(Math.random() * nepons.length)];
             await ctx.reply(idk, {
                 reply_to_message_id: ctx.msg.message_id,
             });
         }
-        logger.error('Unable to get response: ', error);
+
         return;
     }
 
-    // Remove bot's name from the beginning of the reply
-    response = response.trim().replaceAll(/^.+\(@\w+\):/gm, '');
-    response = response.trim().replaceAll(/^Слюша:/gm, '');
+    response = removeBotName(
+        response,
+        bot.botInfo.first_name,
+        bot.botInfo.username,
+    );
+    response = response.replace(
+        /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+        '',
+    );
+    response = response.replace(/\s+/g, ' ');
+    response = response.replace(/^\(in reply.+?\)/gi, '');
 
-    logger.info('last msg: ', message.text);
-    logger.info('reply: ' + response);
+    logger.info(`Response: "${response}"`);
 
     let res;
     try {
@@ -330,18 +443,20 @@ bot.on('message', async (ctx) => {
     }
 
     history.push({
+        id: res.message_id,
         sender: {
             id: bot.botInfo.id,
             name: bot.botInfo.first_name,
             username: bot.botInfo.username,
             myself: true,
         },
-        replyTo: message,
         date: res.date,
-        text: response,
+        text: response.replace(/[\n\r]/g, ' '),
+        isSummary: false,
     });
 
     chat.history = history;
+    chat.lastUse = Date.now();
 });
 
 void bot.start({ drop_pending_updates: false });
@@ -349,35 +464,30 @@ void bot.start({ drop_pending_updates: false });
 function handleMessage(
     msg: Message & Update.NonChannel,
 ): ChatMessage | undefined {
-    let { text } = msg;
-    if (!text && msg.sticker) {
-        text = 'Sticker ' + msg.sticker.emoji;
-    }
+    const text = getText(msg, bot.botInfo.id);
 
     if (!text) return;
-
-    // Slice long messages, which are too long
-    if (text.length > 800) text = text.slice(0, 797) + '...';
 
     let replyTo: ChatMessage['replyTo'] | undefined;
     const reply = msg.reply_to_message;
 
-    if (reply?.from && (reply.caption || reply.text || reply.sticker?.emoji)) {
-        const text = reply.caption ||
-            reply.text ||
-            `Sticker ${reply.sticker?.emoji}`;
+    if (reply?.from && (reply.caption || reply.text || reply.sticker)) {
+        const replyText = getText(reply, bot.botInfo.id);
 
-        replyTo = {
-            sender: {
-                id: reply.from.id,
-                name: reply.from.first_name,
-                username: reply.from.username,
-            },
-            text,
-        };
+        if (replyText) {
+            replyTo = {
+                sender: {
+                    id: reply.from.id,
+                    name: reply.from.first_name,
+                    username: reply.from.username,
+                },
+                text: replyText,
+            };
+        }
     }
 
     return {
+        id: msg.message_id,
         sender: {
             id: msg.from.id,
             name: msg.from.first_name,
@@ -386,36 +496,48 @@ function handleMessage(
         replyTo,
         date: msg.date,
         text,
+        isSummary: false,
     };
 }
 
 function shouldReply(
+    chat: Chat,
     text: string,
     replyFromId: number | undefined,
     chatType: string,
+    replyToId: number | undefined,
 ): boolean {
     const direct =
         // Message reply
         replyFromId === bot.botInfo.id ||
         // Mentioned name
-        text.match(new RegExp(NAMES.join('|'), 'gmi')) ||
+        new RegExp(`(${NAMES.join('|')})`, 'gmi').test(text) ||
         // PM
         chatType === 'private';
 
     const interested =
         // Messages with some special text
-        text.match(new RegExp(`(${tendToReply.join('|')})`, 'gmi')) &&
+        new RegExp(`(${tendToReply.join('|')})`, 'gmi').test(text) &&
         getRandomInt(0, 100) > 100 - 30;
 
-    const ignored =
+    const ignored = (
         // Short message with boring text
-        text.length < 10 &&
-        text.match(new RegExp(`^(${tendToIgnore.join('|')})`, 'gmi')) &&
-        getRandomInt(0, 100) > 100 - 80;
+        text?.length < 30 &&
+        new RegExp(`^(${tendToIgnore.join('|')})`, 'gmi').test(text) &&
+        getRandomInt(0, 100) > 100 - 10
+    )
 
-    const formula = (direct || interested) && !ignored;
+    logger.info(
+        `Deciding on reply "${
+            sliceMessage(text, 20)
+        }": Direct: ${direct}, Interested: ${interested}, Ignored: ${ignored}`,
+    );
 
-    return Boolean(formula);
+    if (direct && !ignored) {
+        return true;
+    }
+
+    return interested && !ignored;
 }
 
 class Typer {
