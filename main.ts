0babc8f85e1ce6b15fc20eeda0a6879304788afc
Sleroq <hireme@sleroq.link>
2024-08-25 19:25:49 +0300

    Commented out replies cause are not helpful

--- main.ts
+++ main.ts
@@ -542,12 +542,12 @@ async function replyWithMarkdown(ctx: Context, text: string) {
     let res;
     try {
         res = await ctx.reply(text, {
-            reply_to_message_id: ctx.msg?.message_id,
+            // reply_to_message_id: ctx.msg?.message_id,
             parse_mode: 'Markdown',
         });
     } catch (_) { // Retry without markdown
         res = await ctx.reply(text, {
-            reply_to_message_id: ctx.msg?.message_id,
+            // reply_to_message_id: ctx.msg?.message_id,
         });
     }
     return res;
