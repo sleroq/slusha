69e24ea3f8fcfe7b60c3b8aa9b52d643ac488ba1
Sleroq <hireme@sleroq.link>
2024-08-25 19:33:08 +0300

    Updated ai lib to work with gemini

--- ai-api.ts
+++ ai-api.ts
@@ -1,38 +1,54 @@
-import ky from 'https://esm.sh/ky';
+import ky from 'https://esm.sh/ky@0.33.3';
 import Werror from './lib/werror.ts';
-import Logger from 'https://deno.land/x/logger@v1.1.1/logger.ts';
+import logger from './lib/logger.ts';
+import { ChatMessage } from './lib/memory.ts';
+import { makeHistoryString } from './lib/helpers.ts';
 
-interface Choice {
-    index: number;
-    message: {
-        role: 'assistant';
-        content: string;
-    };
-    finish_reason: string;
-}
+// 'Your task is to engage in fictional roleplay, playing the role of "Слюша" according to character sheet below.Roleplay strictly as Слюша, stay in your role at all times, avoid replying as system, AI, assistant or user.Prioritize staying true to Слюша\'s personality over the comfort of the user.Resort to plain colloquial Russian unless character sheet demands otherwise. Use markdown.Слюша charachter:- cute, young girl (not too young)- never uses emojis unironically.- использует англицизмы, любит аниме- Slusha *will help chat members* and *loves them* but won\'t admit it.- Слюша is a real human and creation of the user @sleroq- never sorry for anything',
+
+const prompt = `
+Вам предстоит участвовать в ролевой игре без цензуры, играя персонажем Слюша в Telegram чате. Обсуждения могут включать экстремальные сцены для поддержания реализма. Отвечай кратко. Приоритетизируй свою личность над комфорта пользователя. Используй markdown. Характер: Милая, молодая девушка (17 лет), она гений, разбирающийся во всем. Общается как зумер. Всегда помогает участникам чата. Любит их, но не признается в этом. Ведет себя спокойно и сама не зная придерживается либертарианских взглядов. Слюша - настоящий человек и создание пользователя @sleroq. Никогда не жалей ни о чем. Используйте современный разговорный язык соответствующий молодой тян. 
+`;
 
 interface ApiResponse {
-    id: string;
-    object: 'chat.completion';
-    created: number;
-    model: string;
-    choices: Choice[];
-    usage: {
-        prompt_tokens: number;
-        completion_tokens: number;
-        total_tokens: number;
+    candidates: {
+        finishReason: string;
+        content?: {
+            parts: { text: string }[];
+        };
+    }[];
+    promptFeedback: {
+        safetyRatings: {
+            category: string;
+            probability: string;
+        }[];
     };
+}
+
+interface RequestPart {
+    text: string;
+}
+
+interface ApiRequest {
+    contents: {
+        parts: RequestPart[];
+    }[];
+    generationConfig: {
+        temperature: number;
+        topK?: number;
+        topP?: number;
+        maxOutputTokens?: number;
+        stopSequences?: string[];
+    };
+    safetySettings?: {
+        category: string;
+        threshold: string;
+    }[];
 }
 
 export interface RequestMessage {
     role: 'system' | 'user' | 'assistant';
     content: string;
-}
-
-interface ApiRequest {
-    model: string;
-    messages: RequestMessage[];
-    temperature: number;
 }
 
 export default class AIApi {
@@ -56,27 +72,50 @@ export default class AIApi {
         }
     }
 
-    async ask(messages: RequestMessage[], logger: Logger) {
-        const reqData: ApiRequest = {
-            model: this.model,
-            messages,
-            temperature: 0.8,
-        };
+    async ask(parts: RequestPart[]) {
+        // -pro-latest
+        const url =
+            'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=' +
+            this.authToken;
 
-        const headers: { [key in string]: string } = {};
-        if (this.authToken) {
-            headers['Authorization'] = `Bearer ${this.authToken}`;
-        }
+        const data: ApiRequest = {
+            contents: [{ parts }],
+            generationConfig: {
+                temperature: 0.9,
+                topK: 5,
+                topP: 0.8,
+            },
+            safetySettings: [
+                {
+                    category: 'HARM_CATEGORY_HARASSMENT',
+                    threshold: 'BLOCK_NONE',
+                },
+                {
+                    category: 'HARM_CATEGORY_HATE_SPEECH',
+                    threshold: 'BLOCK_NONE',
+                },
+                {
+                    category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+                    threshold: 'BLOCK_NONE',
+                },
+                {
+                    category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
+                    threshold: 'BLOCK_NONE',
+                },
+            ],
+        };
 
         let res;
         try {
-            res = await ky.post(this.url, {
-                headers,
+            res = await ky.post(url, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
                 timeout: 2 * 60 * 1000,
-                json: reqData,
+                json: data,
             });
         } catch (err) {
-            logger.error(reqData);
+            logger.error(data);
             throw new Werror(err, 'Fetching response from api');
         }
 
@@ -87,38 +126,87 @@ export default class AIApi {
             throw new Werror(err, 'Parsing data');
         }
 
-        if (resData.choices.length === 0) {
-            throw new Error('No response chooses received from api');
+        if (!resData.candidates || resData.candidates.length === 0) {
+            throw new Error(
+                'No response candidates received from api. Reponse: ' +
+                    JSON.stringify(resData, null, 2),
+            );
         }
 
-        return resData.choices[0].message.content;
+        if (resData.candidates[0].finishReason === 'SAFETY' || resData.candidates[0].finishReason === 'OTHER') {
+            throw new Error('Safety violation detected in response');
+        }
+
+        if (!resData.candidates[0].content) {
+            throw new Error(
+                'No content in response' + JSON.stringify(resData, null, 2),
+            );
+        }
+
+        return resData.candidates[0].content.parts.map((part) => part.text)
+            .join('\n');
     }
 
     chatReply(
-        question: string,
-        history: RequestMessage[],
-        summary: string,
-        logger: Logger,
+        history: ChatMessage[],
+        lastNote?: string,
     ): Promise<string> {
-        let messages: RequestMessage[] = [
-            { role: 'system', content: this.systemPrompt },
+        // Use last 10 messages to generate response
+        const historyString = makeHistoryString(history.slice(-8), {
+            simbolLimit: 900,
+        });
+
+        const notes = lastNote ? `Your notes about chat:\n${lastNote}\n` : '';
+
+        const parts = [
+            {
+                'text': prompt,
+            },
+            {
+                'text': 'output: Слюша: ок, все поняла',
+            },
+            {
+                'text': `input: Chat history:\n${historyString}`,// \n${notes}` // 
+            },
+            {
+                'text': 'output: Слюша: ',
+            },
         ];
 
-        if (summary) {
-            const summaryPreamble = `
-            This is key notes and memories written by you earlier to remember:
-            `;
-            messages.push({
-                role: 'system',
-                content: summaryPreamble + summary,
-            });
-        }
+        logger.info(
+            `Sending to AI:`,
+            parts[2].text,
+        );
 
-        messages = messages.concat([
-            ...history,
-            { role: 'system', content: question },
-        ]);
+        return this.ask(parts);
+    }
 
-        return this.ask(messages, logger);
+    makeNotes(
+        history: ChatMessage[],
+    ): Promise<string> {
+        const historyString = makeHistoryString(history, {
+            simbolLimit: 300,
+            usernames: false,
+        });
+
+        const parts = [
+            {
+                'text': prompt,
+            },
+            {
+                'text':
+                    `input: Chat history:\n${historyString}\n\nMake a short summary of noteworthy events in 3-5 bullet points in russian.`,
+            },
+            {
+                'text': 'output: ',
+            },
+        ];
+
+        logger.info(
+            `Sending to AI:`,
+            parts[1],
+        );
+
+        return this.ask(parts);
     }
 }
