35a0228755d2874740bd9d93cd8a77700d1e230b
Sleroq <hireme@sleroq.link>
2024-09-27 20:04:54 +0300

    Improved build script

--- scripts/build.bash
+++ scripts/build.bash
@@ -6,10 +6,31 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$(dirname "$SCRIPTPATH")"
 
-deno compile \
-	--output 'out/slusha' \
-	--target 'x86_64-unknown-linux-gnu' \
-	--allow-net \
-	--allow-env \
-	--allow-write \
-	--allow-read main.ts
+# Targets with extensions
+targets=(
+    "x86_64-unknown-linux-gnu"
+    "x86_64-apple-darwin"
+    "aarch64-apple-darwin"
+    "x86_64-pc-windows-msvc"
+)
+
+# Extensions for each target
+extensions=(
+    ""
+    "apple-x86_64"
+    "apple-aarch64"
+    "pc-windows-msvc"
+)
+
+for i in "${!targets[@]}"; do
+    target=${targets[i]}
+    ext=${extensions[i]}
+
+    deno compile \
+        --output "out/slusha$ext" \
+        --target "$target" \
+        --allow-net \
+        --allow-env \
+        --allow-write \
+        --allow-read main.ts
+done
