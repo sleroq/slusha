7340cc41a10b4b00681e569cce695d255df94cad
Sleroq <hireme@sleroq.link>
2025-01-26 15:39:26 +0300

    Updated run script

--- scripts/run.bash
+++ scripts/run.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
-
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$(dirname "$SCRIPTPATH")"
 
 source ./scripts/env.bash
 
-deno run --allow-net --allow-env --allow-write --allow-read main.ts
+while true; do
+    deno run --allow-net --allow-env --allow-write --allow-read --allow-import main.ts
+done
