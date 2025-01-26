02bfa39f222a676088f49ad16a3348a533f1c9f6
Sleroq <hireme@sleroq.link>
2025-01-26 18:59:20 +0300

    Allow exiting with ctrl+c

--- scripts/run.bash
+++ scripts/run.bash
@@ -6,6 +6,8 @@ cd "$(dirname "$SCRIPTPATH")"
 
 source ./scripts/env.bash
 
+trap 'break' SIGINT
+
 while true; do
     deno run --allow-net --allow-env --allow-write --allow-read --allow-import main.ts
 done
