c3d009ba4eb4953a57bc7c24ea3fb65c5228ab5b
Sleroq <hireme@sleroq.link>
2026-07-12 17:42:06 +0300

    Added telegram webapps library

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+    import "./layout.css";
+    import favicon from "$lib/assets/favicon.svg";
 
-	let { children } = $props();
+    let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+    <link rel="icon" href={favicon} />
+    <script src="https://telegram.org/js/telegram-web-app.js?62"></script>
+</svelte:head>
+
 {@render children()}
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        console.log("Telegram web app init data:");
+        console.log(Telegram.WebApp.initData);
+    });
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
